ad3ccd4a2cbbef469c09c2adacc71bc56d5c36b5
Alice DI NUNNO <alice@dinunno.fr>
2024-03-12 22:32:06 -0400

    [fix] profile api language can now be used from middleware

--- back/src/adapters/http/gin/languages.go
+++ back/src/adapters/http/gin/languages.go
@@ -1,10 +1,39 @@
 package gin
 
-import "github.com/gin-gonic/gin"
+import (
+	"back/src/core/domain"
+	"github.com/gin-gonic/gin"
+)
 
 // #YC-8 TODO: move to status route
 func (server *ServiceHTTPServer) getLanguagesHandler(context *gin.Context) {
 	languages := server.ucs.GetAvailableLanguages()
 
 	context.JSON(200, languages)
+}
+
+func (server *ServiceHTTPServer) getLangMiddleware() gin.HandlerFunc {
+	return func(context *gin.Context) {
+		lang := ""
+
+		// priority to accept-language header over user profile so an api call can override the set language if needed
+		acceptLanguage := context.GetHeader("Accept-Language")
+		if acceptLanguage != "" {
+			lang = acceptLanguage
+		} else {
+			userID, exists := context.Get("user")
+			if exists {
+				user, err := server.ucs.GetProfileByUserID(userID.(domain.UserID))
+				if err == nil {
+					lang = user.Language
+				}
+			}
+		}
+
+		if lang == "" {
+			lang = "enUS"
+		}
+
+		context.Set("lang", lang)
+	}
 }
